58384da883805d4f57e7ac6d667bba4b2b12229c
Hà Duy Khánh <ninggiangboy@gmail.com>
2023-02-21 17:54:18 +0700

    Update Inputted.java

--- J1.S.P0074/src/Calculator/view/Inputted.java
+++ J1.S.P0074/src/Calculator/view/Inputted.java
@@ -10,32 +10,26 @@ public class Inputted {
     private static final Scanner INPUT = new Scanner(System.in);
     private static final String NUMBER_VALID = "^-?\\d+$";
 
-    public static int inputInt(String message, int min, int max) {
+    public static int inputInt(String msg, int min, int max) {
         if (min > max) {
             max = max + min;
             min = max - min;
             max = max - min;
         }
 
-        System.out.print(message);
+        System.out.print(msg);
         int inputUser;
 
         do {
             try {
-                String input = INPUT.nextLine().trim();
-                if (!input.matches(NUMBER_VALID)) {
-                    throw new Error();
-                }
-                inputUser = Integer.parseInt(input);
+                inputUser = Integer.parseInt(input.nextLine().trim());
 
                 if (inputUser < min || inputUser > max) {
-                    throw new NumberFormatException();
+                    System.err.println("Input out of range. Enter a value between " + min + " and " + max + ":");
                 } else
                     return inputUser;
-            } catch (Error e) {
-                System.out.println("Nhap so di bay");
             } catch (NumberFormatException e) {
-                System.err.println("Input out of range. Enter a value between " + min + " and " + max + ":");
+                System.err.println("Invalid integer.");
             }
             System.out.print("Enter again: ");
         } while (true);
